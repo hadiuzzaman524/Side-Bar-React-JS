2411f4bb61544e9e248d844237581d90fb3ee7e6
Md Hadiuzzaman <hadi@heavytask.com>
2022-07-13 11:42:24 +0600

    learn : learn about react component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-
-import ReactDOM from 'react-dom'
-
+import React from "react";
+import ReactDOM from "react-dom";
+/*
 function tick(){
 
   const element= (
@@ -13,3 +13,29 @@ function tick(){
 }
 
 setInterval(tick, 1000);
+
+*/
+// React component
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="jaman" />
+      <Welcome name="habib" />
+      <Welcome name="Rakib" />
+    </div>
+  );
+}
+//ReactDOM.render(<App/>, document.getElementById('root'));
+
+class A extends React.Component{
+  render(){
+    return <h1>Hello world {this.props.age}</h1>;
+  }
+}
+
+const root = ReactDOM.render(<A age= '32' />, document.getElementById("root"));
