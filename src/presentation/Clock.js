d9ad668955ebbff35e09b14450a426b08ee1621d
Md Hadiuzzaman <hadi@heavytask.com>
2022-07-14 18:46:53 +0600

    learn : learn about state

--- src/presentation/Clock.js
+++ src/presentation/Clock.js
@@ -1,11 +1,29 @@
 import React from "react";
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { date: new Date() };
+  }
+
+  ticker() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  componentDidMount() {
+    setInterval(()=>this.ticker(), 1000);
+  }
+
+  componentWillUnmount() {}
+
   render() {
     return (
       <div>
         <h1>This is hadi</h1>
-        <h1> {new Date().toLocaleTimeString()}</h1>
+        <h1> {this.state.date.toLocaleTimeString()}</h1>
       </div>
     );
   }
