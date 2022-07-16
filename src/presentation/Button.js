cbd2d6d1cb6865ff3930377d1b3cac6227fe3884
Md Hadiuzzaman <hadi@heavytask.com>
2022-07-16 11:06:46 +0600

    learn: learn about how to stop rendering

--- src/presentation/Button.js
+++ src/presentation/Button.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 class Button extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    const { click: currentClick } = this.props;
+    const { click: nextClick } = nextProps;
+
+    if (currentClick === nextClick) {
+      return false;
+    }
+    return true;
+  }
+
   render() {
-    const { click , name} = this.props;
+    const { click, name } = this.props;
     console.log("Button Rendered");
     return (
       <button type="button" onClick={click}>
