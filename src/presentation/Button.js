e823a4b3ede485340722fc1832a4ca64fb582c32
Md Hadiuzzaman <hadi@heavytask.com>
2022-07-16 10:38:06 +0600

    parameter passing

--- src/presentation/Button.js
+++ src/presentation/Button.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-class Button extends React.Component{
-
-    render(){
-        const {click}= this.props;
-        return (
-            <button type="button" onClick={click}> Hello Hadi</button>
-        );
-    }
+class Button extends React.Component {
+  render() {
+    const { click , name} = this.props;
+    console.log("Button Rendered");
+    return (
+      <button type="button" onClick={click}>
+        {name}
+      </button>
+    );
+  }
 }
 
 export default Button;
--- src/presentation/Clock.js
+++ src/presentation/Clock.js
@@ -4,8 +4,7 @@ import Button from "./Button";
 class Clock extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { date: new Date() };
+    this.state = { date: new Date(), locale: "bn-BD" };
   }
 
   ticker() {
@@ -13,27 +12,32 @@ class Clock extends React.Component {
       date: new Date(),
     });
   }
-// componentDidMount call when previous object show in DOM then call it
+  // componentDidMount call when previous object show in DOM then call it
   componentDidMount() {
-    // save interval into timer for clear 
-   this.timer= setInterval(()=>this.ticker(), 1000);
+    // save interval into timer for clear
+    this.timer = setInterval(() => this.ticker(), 1000);
   }
 
-  // componentWillUnmount call when page will be destroyed or changed 
+  // componentWillUnmount call when page will be destroyed or changed
   componentWillUnmount() {
     // clear the interval time for avoid memory lacking
     clearInterval(this.timer);
   }
 
-  click = ()=> {
-    console.log('click');
-  }
+  click = () => {
+    this.setState({
+      locale: "en-US",
+    });
+    console.log(this.state.locale);
+  };
+
   render() {
+    console.log("Clock Rendered");
     return (
       <div>
         <h1>This is hadi</h1>
-        <h1> {this.state.date.toLocaleTimeString()}</h1>
-        <Button click={this.click}></Button>
+        <h1> {this.state.date.toLocaleTimeString(this.state.locale)}</h1>
+        <Button click={this.click} name="Change Time"></Button>
       </div>
     );
   }
