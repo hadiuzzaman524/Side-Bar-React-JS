846654e394f74ca3cfeb4060075760f2959e7e57
Md Hadiuzzaman <hadi@heavytask.com>
2022-08-22 20:03:33 +0600

    feat: initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Clock from "./presentation/Clock";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(<Clock/>, document.getElementById('root')); 
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
